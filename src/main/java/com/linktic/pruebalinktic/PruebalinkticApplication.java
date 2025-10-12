6a2e86886655ecb9207160cec7741e9f417a0050
Carlos Steven Vargas Hernandez <steven17.v@gmail.com>
2025-10-11 19:32:30 -0500

    Primer commit

---
+++ src/main/java/com/linktic/pruebalinktic/PruebalinkticApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.pruebalinktic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebalinkticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebalinkticApplication.class, args);
+	}
+
+}
